bba777d5142e88ba77a10721b9b62d002b95cab6
Yegor Men <yegormenovchshikov@gmail.com>
2025-06-20 00:19:30 +1000

    Modular Architecture
    I think the math is over with? Not sure, seems solid for now. Need to link to the previous layer and stuff to make work

--- complex_architecture.py
+++ complex_architecture.py
@@ -20,9 +20,11 @@ class Layer:
 
         self.w = torch.randn(num_in, num_out).to(device) * 0.1
         self.mem = torch.zeros(num_out).to(device)
+        self.mem_old = torch.zeros(num_out).to(device)
         self.mem_decay = torch.ones(num_out).to(device) * mem_decay
         self.threshold = torch.ones(num_out).to(device) * threshold
         # Threshold is nicer as positive, so we pass it through softplus before use
+        # Derivative of softplus is sigmoid, how convenient :)
 
         self.input_trace = torch.zeros(num_in).to(device)
         self.output_trace = torch.zeros().to(device)
@@ -48,6 +50,7 @@ class Layer:
         # sigmoid is easiest as it's built in, also the derivative is easy as hell
         # s(x) = e^x / (1 + e^x)
         # s'(x) = [s(x)][1 - s(x)]
+        # s'(x) = 1 / (1 + e^(-x))
 
         # "firing" is approximated via the sigmoid of the mem level
         # round(s(mem - threshold)) = {0, 1}
@@ -60,14 +63,31 @@ class Layer:
 
         # need to calculate the derivative of all learnable parameters
         # d_w, d_mem_decay, d_thresh
-        # also need to calculate the derivative with respect to the input to pass as the learning signal to the previous layer
+        # also need to calculate the derivative with respect to the input to pass as the learning signal to the upstream layer
         # d_i
         
         # Hence, the output function can be calculated like this:
         # o(mem, t) = e^(mem - t) / (1 + e^(mem - t))
         # o(mem_old, mem_decay, t) = e^(mem_old * mem_decay + current - t) / (1 + e^(mem_old * mem_decay + current - t))
         # o(mem_old, mem_decay, t, i, w) = e^(mem_old * mem_decay + i*w - t) / (1 + e^(mem_old * mem_decay + i*w - t))
+        # Say that x = mem_old * mem_decay + i*w - t, in that case:
+        # o(x) = e^x / (1 + e^x)
+        # o'(x) = d_o/d_x = [o(x)][1 - o(x)] = 1 / (1 + o^(-x)) = SOME NUMBER
+        # We want to use the chain rule, so we already have a number for d_loss/d_o, we just did d_o/dx,
+        # We know that x = mem_old * mem_decay + i*w - t
+        # So now we need d_x with respect to mem_decay, i, w, t
+        # d_x/d_mem_decay = mem_old
+        # d_x/d_i = w
+        # d_x/d_w = i
+        # d_x/d_t = derivative of softplus = sigmoid(t) = S(t) * -1
         
+        # Therefore, we have:
+        # Gradient of w: d_loss/d_w = (d_loss/d_o) * (d_o/d_x) * (d_x/d_w) = LS * S(mem_new - t)/(1 - S(mem_new - t)) * i
+        # Gradient of t: d_loss/d_w = (d_loss/d_o) * (d_o/d_x) * (d_x/d_w) = LS * S(mem_new - t)/(1 - S(mem_new - t)) * S(t) * -1
+        # Gradient of mem_decay: d_loss/d_w = (d_loss/d_o) * (d_o/d_x) * (d_x/d_w) = LS * S(mem_new - t)/(1 - S(mem_new - t)) * mem_old
+        # LS for upstream layer: d_loss/d_w = (d_loss/d_o) * (d_o/d_x) * (d_x/d_w) = LS * S(mem_new - t)/(1 - S(mem_new - t)) * w
+        
+        # Then we just subtract the gradients from whatever we have, and that's an update to the parameters
 
         pass
 
@@ -84,6 +104,7 @@ class Layer:
         self.update_parameters()
 
         raw_current = in_spikes @ self.w
+        self.mem_old = self.mem
         self.mem *= self.mem_decay
         self.mem += raw_current
 
